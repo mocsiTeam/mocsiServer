05b2f5b200fa851f40fd2f6efc6bd2a4200f2af8
p1ck0 <baierilja@ya.ru>
2020-11-17 23:32:24 +0100

    [MOCSI-15](Server DB):fixed structures

--- db/structs.go
+++ db/structs.go
@@ -7,8 +7,8 @@ import (
 )
 
 type Roles struct {
-	ID   uint   `gorm: primaryKey`
-	Name string `gorm: unique;not null`
+	ID       uint   `gorm: primaryKey`
+	RoleName string `gorm: unique;not null`
 }
 
 type Groups struct {
@@ -37,31 +37,41 @@ type Users struct {
 	Name       string
 	SurName    string
 	SecondName string
-	Role       Roles `gorm:"foreignKey:Name;not null"`
+	RoleID     uint
+	Role       Roles `gorm:"foreignKey:RoleID"`
 }
 
 type UserGroups struct {
-	ID    uint   `gorm: primaryKey`
-	User  Users  `gorm:"foreignKey:NickName;not null"`
-	Group Groups `gorm:"foreignKey:Name;not null"`
+	ID      uint `gorm: primaryKey`
+	UserID  uint
+	GroupID uint
+	User    Users  `gorm:"foreignKey:UserID";not null`
+	Group   Groups `gorm:"foreignKey:GroupID";not null"`
 }
 
 type RoomAccess struct {
-	ID    uint        `gorm: primaryKey`
-	User  Users       `gorm:"foreignKey:NickName;not null"`
-	Room  Rooms       `gorm:"foreignKey:Name;not null"`
-	Level AccessLevel `gorm:"foreignKey:Name;not null"`
+	ID      uint `gorm: primaryKey`
+	UserID  uint
+	RoomID  uint
+	LevelID uint
+	User    Users       `gorm:"foreignKey:UserID";not null`
+	Room    Rooms       `gorm:"foreignKey:RoomID";not null`
+	Level   AccessLevel `gorm:"foreignKey:LevelID";not null`
 }
 
 type StatsRoom struct {
-	ID         uint  `gorm: primaryKey`
-	Room       Rooms `gorm:"foreignKey:Name;not null"`
+	ID         uint `gorm: primaryKey`
+	RoomID     uint
+	Room       Rooms `gorm:"foreignKey:RoomID";not null`
 	CountUsers int
 }
 
 type StatsUser struct {
 	ID        uint `gorm: primaryKey`
 	CreatedAt time.Time
-	Room      Rooms `gorm:"foreignKey:Name;not null"`
+	RoomID    uint
+	Room      Rooms `gorm:"foreignKey:RoomID";not null`
+	UserID    uint
+	User      Users `gorm:"foreignKey:UserID";not null`
 	TimeSpend time.Time
 }
